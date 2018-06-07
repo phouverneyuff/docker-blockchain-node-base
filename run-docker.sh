5392e575fdff69029e843a87cfe39ef3af188da3
phouverney <ph.ouverney@id.uff.br>
2018-06-07 00:22:43 -0300

    improving run-docker

--- run-docker.sh
+++ run-docker.sh
@@ -2,4 +2,8 @@
 
 source env.sh
 
-docker run -it $DOCKER_REPO/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION /bin/bash
+if [[ -n $(docker images $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION | sed -n 2p) ]]; then
+    docker run -it $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION /bin/bash 
+else
+    docker run -it $DOCKER_REPO/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION /bin/bash
+fi
